084e9f23d3bd8544295b2096e805e52a53a7737d
Samie Mostafavi <ssmos@kth.se>
2024-08-09 09:40:46 +0000

    offline process debugged

--- offline_edaf.py
+++ offline_edaf.py
@@ -1,4 +1,4 @@
-import sys, gzip, json
+import os, sys, gzip, json
 from pathlib import Path
 from loguru import logger
 from collections import deque
@@ -9,6 +9,10 @@ from edaf.core.uplink.ue import ProcessULUE
 from edaf.core.uplink.nlmt import process_ul_nlmt
 from edaf.core.uplink.combine import CombineUL
 from edaf.core.uplink.decompose import process_ul_journeys
+    
+if not os.getenv('DEBUG'):
+    logger.remove()
+    logger.add(sys.stdout, level="INFO")
 
 NUM_POP = 10000
 
@@ -49,9 +53,6 @@ class RingBuffer:
 
 if __name__ == "__main__":
 
-    logger.remove()
-    logger.add(sys.stdout, level="INFO")
-
     if len(sys.argv) != 3:
         logger.error("Usage: python offline_edaf.py <source_folder_address> <result_parquetfile>")
         sys.exit(1)
@@ -61,11 +62,11 @@ if __name__ == "__main__":
     result_parquet_file = Path(sys.argv[2])
 
     gnb_path = folder_path.joinpath("gnb")
-    gnb_lseq_file = list(gnb_path.glob("latseq.*.lseq"))[0]
+    gnb_lseq_file = list(gnb_path.glob("*.lseq"))[0]
     logger.info(f"found gnb lseq file: {gnb_lseq_file}")
 
     ue_path = folder_path.joinpath("ue")
-    ue_lseq_file = list(ue_path.glob("latseq.*.lseq"))[0]
+    ue_lseq_file = list(ue_path.glob("*.lseq"))[0]
     logger.info(f"found ue lseq file: {ue_lseq_file}")
 
     upf_path = folder_path.joinpath("upf")
@@ -104,20 +105,29 @@ if __name__ == "__main__":
     ind = 0
     df = pd.DataFrame()
     while True:
-        df_combined = combineul.run(
-            nlmt_journeys[ind:ind+NUM_POP],
-            gnb_journeys[ind:ind+NUM_POP],
-            ue_journeys[ind:ind+NUM_POP]
-        )
+        if ind+NUM_POP > len(nlmt_journeys) or ind+NUM_POP > len(nlmt_journeys) or ind+NUM_POP > len(nlmt_journeys):
+            # process all the rest
+            df_combined = combineul.run(
+                nlmt_journeys[ind:-1],
+                gnb_journeys[ind:-1],
+                ue_journeys[ind:-1]
+            )
+        else:
+            # process a batch of size NUM_POP
+            df_combined = combineul.run(
+                nlmt_journeys[ind:ind+NUM_POP],
+                gnb_journeys[ind:ind+NUM_POP],
+                ue_journeys[ind:ind+NUM_POP]
+            )
+
         logger.info(f'Combined len: {len(df_combined)}')
-
-        ind += NUM_POP
-        if ind > len(nlmt_journeys) or ind > len(nlmt_journeys) or ind > len(nlmt_journeys):
-            break
-
         df_to_append = process_ul_journeys(df_combined)
         logger.info(f'Processed len: {len(df_to_append)}')
         df = pd.concat([df, df_to_append], ignore_index=True)
+        
+        ind += NUM_POP
+        if ind > len(nlmt_journeys) or ind > len(nlmt_journeys) or ind > len(nlmt_journeys):
+            break
     
     logger.info(f"Combines logs, created a df with {len(df)} entries.")
     df.to_parquet(result_parquet_file, engine='pyarrow')
